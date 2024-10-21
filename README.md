f1c97012c0b2bd060e0812cc21e0ea9400ab4009
NouraHoumanii <noura.houmani@net.usj.edu.lb>
2024-10-21 13:05:36 +0300

    first commit

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 "# DevoirAndroid" 
 "# DevoirAndroid" 
 "# AndroidAssgnment" 
+"# AndroidDev" 
--- app/src/main/java/com/example/loginactivity/MainActivity.kt
+++ app/src/main/java/com/example/loginactivity/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)git remote -v
+
 
         val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
